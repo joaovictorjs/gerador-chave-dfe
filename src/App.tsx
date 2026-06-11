d424cfcccd98a6feda9d3f90a78755f7e4cc89ea
joaovictorjs <skai7ami@gmail.com>
2026-06-11 14:55:57 -0300

    chore: adiciona campos do emitente

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import CamposEmitente from './CamposEmitente';
+
 export default function App() {
-  return <button className="btn">Hello World</button>;
+  return (
+    <form className="absolute top-1/2 left-1/2 flex w-1/3 -translate-1/2 flex-col gap-4">
+      <CamposEmitente />
+    </form>
+  );
 }
