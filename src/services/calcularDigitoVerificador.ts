f614a60cedff6832b8d4c312b4b81647a5b8766e
alyssonoliverr <alyzsorn@gmail.com>
2026-06-12 15:44:27 -0300

    refactor: melhorar validação do cálculo do dv

--- src/services/calcularDigitoVerificador.ts
+++ src/services/calcularDigitoVerificador.ts
@@ -1,28 +1,25 @@
 export function calcularDigitoVerificador(chaveSemDv: string): number {
+  if (!chaveSemDv || !/^\d{43}$/.test(chaveSemDv)) {
+    throw new Error(
+      "A chave deve conter exatamente 43 dígitos numéricos.",
+    );
+  }
 
-      if (!chaveSemDv || !/^\d{43}$/.test(chaveSemDv)) {
-        throw new Error(
-            "A chave não pode ser nula, e deve conter exatamente 43 dígitos numéricos."
-        );
-    } 
+  let soma = 0;
+  let pesoAtual = 2;
 
-        let soma = 0;
-        let pesoAtual = 2;
+  for (let i = chaveSemDv.length - 1; i >= 0; i--) {
+    const digito = Number(chaveSemDv[i]);
+    soma += digito * pesoAtual;
 
-        for(let i = chaveSemDv.length -1; i >= 0; i--){
-            const digito = Number(chaveSemDv[i]);
-            soma += digito * pesoAtual;
-            
-            pesoAtual++;
-            if(pesoAtual > 9){
-                pesoAtual = 2;
-            }
-        }
-
-        const resto = soma % 11;
-        const digitoVerificador = 11 - resto;
-
-            return digitoVerificador >= 10
-        ? 0
-        : digitoVerificador;
+    pesoAtual++;
+    if (pesoAtual > 9) {
+      pesoAtual = 2;
     }
+  }
+
+  const resto = soma % 11;
+  const digitoVerificador = 11 - resto;
+
+  return digitoVerificador >= 10 ? 0 : digitoVerificador;
+}
