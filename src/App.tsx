c2a231c0094371573ede4e0a0d9c060aac78110d
joaovictorjs <skai7ami@gmail.com>
2026-06-13 20:16:46 -0300

    chore: remove dicas

--- src/App.tsx
+++ src/App.tsx
@@ -15,23 +15,6 @@ export default function App() {
           <CamposDocumento />
           <ChaveGerada />
         </form>
-
-        <div className="flex w-full flex-col gap-2 xl:w-1/2">
-          <div role="alert" className="alert bg-base-100">
-            <span>
-              Os campos vazios serão preenchidos com dados aleatórios ao gerar a
-              chave de acesso.
-            </span>
-          </div>
-
-          <div role="alert" className="alert bg-base-100 hidden lg:block">
-            <span>
-              Pressione <kbd className="kbd">Ctrl</kbd> +{' '}
-              <kbd className="kbd">Enter</kbd> para gerar e copiar uma chave de
-              acesso.
-            </span>
-          </div>
-        </div>
       </div>
     </AppProvedor>
   );
