7bb8c6236e60c8736d196176e233b8f1c498c550
joaovictorjs <skai7ami@gmail.com>
2026-09-02 16:57:09 -0300

    feat: sanitiza campos numéricos

--- src/utils.ts
+++ src/utils.ts
@@ -66,12 +66,16 @@ export function gerarChaveAcesso(dados: DadosChaveAcesso): string {
   return `${chaveSemDv}${digitoVerificador}`;
 }
 
+function sanitizarNumeros(valor: string): string {
+  return valor.replace(/\D/g, '');
+}
+
 function montarChaveSemDv(dados: DadosChaveAcesso): string {
   return (
     dados.cUF.toString().padStart(2, '0') +
-    dados.aamm +
-    dados.cnpj.padStart(14, '0') +
-    dados.modelo +
+    sanitizarNumeros(dados.aamm) +
+    sanitizarNumeros(dados.cnpj).padStart(14, '0') +
+    sanitizarNumeros(dados.modelo) +
     dados.serie.toString().padStart(3, '0') +
     dados.numero.toString().padStart(9, '0') +
     dados.tpEmis +
